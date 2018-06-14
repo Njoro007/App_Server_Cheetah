cfe30ac866e7e43badc95bc05f7e8f23814b0e2f
stondiki <stondikiyeye@gmail.com>
2018-06-14 10:39:40 +0300

    waterpanController

--- app-server/src/main/java/com/hackathon/cheetah/appserver/controllers/WaterPanController.java
+++ app-server/src/main/java/com/hackathon/cheetah/appserver/controllers/WaterPanController.java
@@ -2,6 +2,7 @@ package com.hackathon.cheetah.appserver.controllers;
 
 import com.hackathon.cheetah.appserver.models.entities.WaterPan;
 import com.hackathon.cheetah.appserver.models.repositories.WaterpanRepository;
+import com.sun.xml.internal.bind.v2.model.core.ID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,5 +19,18 @@ public class WaterPanController {
     @RequestMapping(method = RequestMethod.GET, value = "")
     public ResponseEntity<List<WaterPan>> waterpans(){
         return ResponseEntity.ok((List<WaterPan>) waterpanRepository.findAll());
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<WaterPan> createWaterPan(@RequestBody WaterPan waterpans) {
+        waterpans=waterpanRepository.save(waterpans);
+        return ResponseEntity.ok(waterpans);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<WaterPan> getWaterPan(@PathVariable Long id) {
+
+        WaterPan waterpan = waterpanRepository.findById(id).get();
+            return ResponseEntity.ok(waterpan);
     }
 }
